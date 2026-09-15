860312a8d1542b13743b54dc64e02848affd0373
fvogelaar <francienvogelaar@gmail.com>
2026-09-15 16:46:06 +0200

    Stateless Class added + implementation Q1

---
+++ Q1_filter_state_F.py
@@ -0,0 +1,26 @@
+"""
+Compare difference between passing filter state (zi)
+and restarting the filter at 0 each chunk
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import butter, filtfilt
+from src.filters import OnlineBandpass, Stateless
+
+FS = 250
+N_CHANNELS = 1
+
+signal = np.random.randn(FS * 5, N_CHANNELS)
+
+stateful_filt = OnlineBandpass(FS, 8, 30, N_CHANNELS)
+stateless = [stateful_filt.process([s])[0] for s in signal[:, 0]]
+
+stateless_filt = Stateless(FS, 8, 30, N_CHANNELS)
+stateful = [stateless_filt.process([s])[0] for s in signal[:, 0]]
+
+plt.plot(stateless, label="stateless (causal)")
+plt.plot(stateful, label="stateful (causal)")
+plt.legend()
+plt.title("Stateful vs stateless Filtering")
+plt.show()
--- src/filters.py
+++ src/filters.py
@@ -25,3 +25,32 @@ class OnlineBandpass:
             [sample], axis=0, zi=self.zi
         )
         return y[0]
+
+
+class Stateless:
+    """
+    For question 1:
+    Stateless causal band-pass filter
+    """
+    def __init__(self, fs, low, high, n_channels, order=4):
+        self.b, self.a = butter(
+            order, [low, high],
+            btype="band", fs=fs
+        )
+        self.zi = np.zeros((max(len(self.a), len(self.b)) - 1, n_channels))
+        self.n_channels = n_channels
+
+
+    def process(self, sample):
+        """
+        Process one multichannel EEG sample
+        """
+        zi = np.zeros(
+            (max(len(self.a), len(self.b)) - 1, self.n_channels)
+        )
+
+        y, _ = lfilter(
+            self.b, self.a,
+            [sample], axis=0, zi=zi)     # note that zi is not self.zi but np.zeros        )
+        return y[0]
+        
