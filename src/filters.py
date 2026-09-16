673b98fd09d8f3223e4d6d35985318e77ed677a0
fvogelaar <francienvogelaar@gmail.com>
2026-09-16 16:11:11 +0200

    added a method to process chunks in OnlineBandpass

--- src/filters.py
+++ src/filters.py
@@ -5,6 +5,7 @@ Causal EEG filters for real-time use
 import numpy as np
 from scipy.signal import butter, lfilter
 
+
 class OnlineBandpass:
     """
     Stateful causal band-pass filter
@@ -26,6 +27,22 @@ class OnlineBandpass:
         )
         return y[0]
 
+    def process_chunk(self, samples):
+        """
+        Process multiple samples at once (chunk).
+
+        Shape:
+        (number_of_samples, number_of_channels)
+        Essentially instead of 1 sample we pass a
+        sample matrix to lfilter.
+        """
+        y, self.zi = lfilter(
+            self.b, self.a,
+            samples, axis=0,
+            zi=self.zi
+        )
+        return y
+
 
 class Stateless:
     """
@@ -40,7 +57,6 @@ class Stateless:
         self.zi = np.zeros((max(len(self.a), len(self.b)) - 1, n_channels))
         self.n_channels = n_channels
 
-
     def process(self, sample):
         """
         Process one multichannel EEG sample
@@ -51,6 +67,6 @@ class Stateless:
 
         y, _ = lfilter(
             self.b, self.a,
-            [sample], axis=0, zi=zi)     # note that zi is not self.zi but np.zeros        )
+            [sample], axis=0, zi=zi)
+        # note that zi is not self.zi but np.zeros
         return y[0]
-        
